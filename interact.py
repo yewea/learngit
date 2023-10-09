cf3473fd4077cc42ce016c3932921d0eb8d01567
yewe <1904804171@qq.com>
2023-10-09 15:17:00 +0800

    添加了注释

--- interact.py
+++ interact.py
@@ -1,3 +1,5 @@
+# 下面这个类，以两个人之间的聊天为对象，其中u1,u2,指的是两个用户的用户名，message则是聊天记录
+
 class Interact(object):
     def __init__(self, a1, a2):
         self.u1 = a1
@@ -6,15 +8,18 @@ class Interact(object):
 
     def add_information(self, sender, information):
         self.messages.append([sender, information])
+# 添加聊天记录，可设置一个记录消息的上限，超过上限的部分可以删除
 
     def del_information(self, sender, information):
         self.messages.remove([sender, information])
+# 删除聊天记录，为暂定方案，还未完善
 
     def show(self):
         n = 0
         while n != len(self.messages):
             print(self.messages[n][0], ':', self.messages[n][1], '\n')
             n += 1
+# 显示聊天记录，暂时用于程序测试
 
 
 class User(object):
